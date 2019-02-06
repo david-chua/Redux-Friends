82311db6bd1171e91d2ebd46be14f3e69d291971
david-chua <dp_chua@hotmail.com>
2019-02-05 22:15:08 -0500

    applied logger middleware

--- friends/src/index.js
+++ friends/src/index.js
@@ -3,14 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import logger from "redux-logger";
 
 import { Provider } from "react-redux";
 
 import { reducer } from "./reducers/reducer";
 
-const store = createStore(reducer)
+const store = createStore(reducer, applyMiddleware(logger))
 
 ReactDOM.render(
   <Provider store={store }>
